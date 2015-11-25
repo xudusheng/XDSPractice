8c17efc1a36bc9922eafa09e0713bfec691569f9
xudusheng <597132100@qq.com>
2015-11-25 17:09:26 +0800

    first commit

---
+++ XDSPractice/SmtaranAD/SmtaranADViewController.h
@@ -0,0 +1,13 @@
+//
+//  SmtaranADViewController.h
+//  XDSPractice
+//
+//  Created by zhengda on 15/11/25.
+//  Copyright © 2015年 zhengda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SmtaranADViewController : UIViewController
+
+@end
